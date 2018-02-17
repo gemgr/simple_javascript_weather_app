2adce5e24a67c1b3db934ed7802e5634faa6afcc
George Moustakas <gemgr@users.noreply.github.com>
2018-02-18 01:27:37 +0200

    code refactoring - improved ui and more. :coffee: :cookie:

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# Simple Javascript & jQuery Weather App  - WORK_IN_PROGRESS -
+![Made with Bulma](https://bulma.io/images/made-with-bulma--semiblack.png) |<a href='https://ko-fi.com/Z8Z19DMP' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+------------ | -------------
+
+# Published under the [MIT](https://github.com/gemgr/simple_javascript_weather_app/blob/master/LICENSE) Licence
+
+## Live Demo: [gemgr.github.io/app/weather/](https://gemgr.github.io/app/weather/)
+
+# About the app:
+The app uses [Open Weather Map](https://openweathermap.org/) to retrieve the current weather for the city you requested, and all of that in **Real-Time** as your write the name of the city the app retrives the wheather.
+
+# Made possible with:
+-  [Open Weather Map](https://openweathermap.org/)
+-  [Bulma Framework](https://bulma.io/)
+-  [Font Awesome](http://fontawesome.io/)
+-  [jQuery](https://jquery.com/)
 
 
-###### Made possible with:
-* :coffee:
-* :sweat:
-* [Materialize CSS](http://materializecss.com/)
-* [Font Awesome](http://fontawesome.io/)
-* [jQuery](https://jquery.com/)
+# Plans:
+### Todo
+- Rewrite all Javascript components with **pure** Modern Javascript
+- Show weather for big Cities by default such as New York, London and more.
+- Add support for 5 day / 3 hour forecast
+- Add support for 16 day / daily forecast 
+- Maybe a Redesign? [Tweet me some Ideas ](https://twitter.com/george_mou)
+
+
+
+
+# Changelog:
+
+### Changed
+- Now you can search the weather on your city **without** submiting any form. All in **Real-Time** because who submits forms this days!
+- Moved from ~~[Materialize CSS Framework](http://materializecss.com/)~~ to [Bulma Framework](https://bulma.io/)
+- Improved UI
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,79 +1,65 @@
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getWeatherForUserLocation, showErrorAfterLocationRequest);
-    showMsg('circle-notch fa-spin', 'Waiting for location access.' , 20000);
-  } else {
-    showMsg('ban', 'Geolocation is not supported by this browser.' , 6000);
-  }
-}
+$(document).ready(function() {
+  $(".search").val("");
+  $(".search").focus();
+  $(".is-custom-color-icon-check").css('display', 'none');
+  $(".is-custom-color-icon-ban").css('display', 'none');
+  $(".is-custom-notification").css('display', 'none');
+});
 
-
-function showErrorAfterLocationRequest(err) {
-  switch(err.code) {
-    case err.PERMISSION_DENIED:
-      showMsg('ban', 'Access to location denied.' , 6000);
-      break;
-    case err.POSITION_UNAVAILABLE:
-      showMsg('ban', 'Location information is unavailable.' , 6000);
-      break;
-    case err.TIMEOUT:
-      showMsg('ban', 'Request for geolocation timed out.' , 6000);
-      break;
-    case err.UNKNOWN_ERROR:
-      showMsg('ban', 'An unknown error occurred.' , 6000);
-      break;
-  }
-}
-
-// DEBUG: Run the function here for now untill the UI is ready
-getLocation()
-
-//
-//  Help Functions of the App includes:
-//  removeToastAfterAction(), showMsg(), getWeatherForUserLocation(),
-//
-//
-
-
-// Remove previous Toast so another toast can be shown or remove it completely
-function removeToastAfterAction() {
-  if ($('.toast').length > 0) {
-    const toastElement = $('.toast').first()[0];
-    const toastInstance = toastElement.M_Toast;
-    toastInstance.remove()
-  }
-}
-
-// Modular Toast Function that takes and icon if any, a message, and a timeout number in ms
-// Show a Toast after and action is taken to inform user of the result
-function showMsg(icon, message, time) {
-  if (icon !== null) {
-    removeToastAfterAction();
-    Materialize.toast(`<i class="fas fa-${icon}"></i> &nbsp; - &nbsp; ${message}`, time);
-  } else {
-    removeToastAfterAction();
-    Materialize.toast(`${message}`, time);
-  }
-}
-
-// Get Weather for users Location
-function getWeatherForUserLocation(position) {
-  showMsg('check', 'Established approximate location.' , 6000);
-
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
+$(".search").on("change keyup paste", function() {
+  $(".is-custom-color-icon-check").css('display', 'none');
+  let = SEARCH_TERM = $(".search").val();
   let UNITS = "metric" // TODO: Check what the user selected and pass it to the url
-  const API_KEY = ''
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${UNITS}&appid=${API_KEY}`
+  const API_KEY = '' // Open Weather Map API KEY
+  const SEARCH_URL = `https://api.openweathermap.org/data/2.5/weather?q=${SEARCH_TERM}&units=${UNITS}&appid=${API_KEY}`
 
-  // DEBUG: Log Lat & Lot GPS Coords
-  console.log(lat, lon)
+  if (!SEARCH_TERM) {
+    $(".is-custom-hero-content p").removeClass("is-loading");
+  }
 
-
-  $.get(URL, function(res) {
-    console.log(res)
-    $('.weather span.temp').html(res['main']['temp'],"C")
-    $('.weather span.city').html(res['name'] +", "+ res['sys']['country'])
+  $(".is-custom-hero-content p").removeClass("is-loading");
+  $(".is-custom-color-icon-ban").css('display', 'flex');
+  $.get(SEARCH_URL, function(res) {
+    if (res) {
+      $(".is-custom-color-icon-ban").css('display', 'none');
+      $(".is-custom-hero-content p").addClass("is-loading");
+      document.getElementById("location_name").innerHTML = res['name'] + ", " + res['sys']['country'];
+      document.getElementById("location_temp").innerHTML = res['main']['temp'];
+      document.getElementById("location_temp_max").innerHTML = res['main']['temp_max'];
+      document.getElementById("location_temp_min").innerHTML = res['main']['temp_min'];
+      $(".is-custom-hero-content p").removeClass("is-loading");
+      $(".is-custom-color-icon-check").css('display', 'flex');
+    } else {
+      $(".is-custom-hero-content p").addClass("is-loading");
+      $(".is-custom-color-icon-ban").css('display', 'flex');
+    }
   });
+});
 
-}
+
+// Bulma Menu Toggler
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function($el) {
+      $el.addEventListener('click', function() {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
